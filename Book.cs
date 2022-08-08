7e50e3a719bd299ba6834558f922c5c5793f5edc
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-08 16:15:14 +1200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningClassesAndObjects
+{
+
+    //this is a class that has been assigned properties - these properties can have values assigned to them.
+    internal class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //this creates a new instance of the class - this is called an object and can now be used similar to a variable
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "J.K. Rowling";
+            book1.pages = 400;
+
+            Book book2 = new Book();
+            book2.title = "Lord of the Rings ";
+            book2.author = "J.R.R. Tolkien ";
+            book2.pages = 650;
+
+            Console.WriteLine(book2.title);
+
+            Console.ReadLine();
+
+        }
+    }
+}
